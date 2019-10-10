b55899d0aaece76b6a02e626402302c9ef0698b2
Jonathan Basseri <misterikkit@gmail.com>
2019-10-09 23:15:55 -0700

    anti-aliasing and sprites are working but there is a memory leak

--- main.go
+++ main.go
@@ -2,56 +2,51 @@ package main
 
 import (
 	"fmt"
+	"image/color"
+	"math"
 	"syscall/js"
 	"time"
 )
 
+const (
+	// Tau is better than Pi.
+	Tau = 2 * math.Pi
+
+	// Frames per second
+	FrameRate = 60
+
+	// Number of pixels in a frame
+	FrameSize = 24
+
+	// Size of a pixel in radians
+	PixelWidth = Tau / FrameSize
+)
+
+// RGB is a color struct that plays nicely with wasm. Will need to move to
+// `color.RGBA` on TinyGo.
 type RGB struct {
 	R, G, B uint8
 }
 
+// JSValue converts the given color to a JS object.
 func (c RGB) JSValue() js.Value {
 	return js.ValueOf(map[string]interface{}{
 		"R": c.R, "G": c.G, "B": c.B,
 	})
 }
 
-type Frame []RGB
+// Frame represents all the pixels in one frame of animation.
+// Should this be [24]RGB?
+type Frame = []RGB
 
-func (f Frame) Copy() Frame {
-	f2 := make(Frame, len(f))
+// newFrame returns an all-black frame.
+func newFrame() Frame { return make(Frame, FrameSize) }
+
+// reset blanks a frame (to avoid allocating new frames)
+func reset(f Frame) {
 	for i := range f {
-		f2[i] = f[i]
+		f[i] = RGB{}
 	}
-	// copy(f2, f)
-	return f2
-}
-
-var blank = Frame{
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
-	{0, 0, 0},
 }
 
 func main() {
@@ -59,40 +54,42 @@ func main() {
 	loaded := make(chan struct{})
 	js.Global().Set("goLoad", js.FuncOf(func(js.Value, []js.Value) interface{} { close(loaded); return nil }))
 	<-loaded
-	DisplayLEDs(blank)
+	DisplayLEDs(newFrame())
 
-	// fadeIn := Tween{from: time.Now(), to: time.Now().Add(4 * time.Second), start: 30, end: -10}
-	updaters := []UpdateFunc{
-		// func(now time.Time) bool {
-		// 	v, ok := fadeIn.Value(now)
-		// 	fmt.Println(v)
-		// 	return ok
-		// },
-		func(now time.Time) bool {
-			// const period = 3 * time.Second
-			// phase := now.Sub(now.Round(period))
-			// frac := float64(phase) / float64(period)
-			// fmt.Println(frac)
-			step := int(time.Second.Nanoseconds()) / 24 // const
-			pixel := float32(now.Nanosecond()) / float32(step)
-			i := int(pixel)
-			weight := 1.0 - (pixel - float32(i))
-			val := uint8(255 * weight)
-			f := blank.Copy()
-			f[i] = RGB{val, val, val}
-			f[(i+1)%len(f)] = RGB{255 - val, 255 - val, 255 - val}
-			DisplayLEDs(f)
-			return true
-		},
+	// Set up animation
+	f := newFrame()
+	var dots []sprite
+	for i := 0; i < 7; i++ {
+		dots = append(dots, sprite{Size: 1.5 * PixelWidth, Color: color.RGBA{0x32, 0x6C, 0xE5, 0}})
 	}
-	t := time.NewTicker(time.Second / 60)
+	const (
+		period = 10 * time.Second
+		divide = Tau / 7.0
+	)
+
+	// Update animation
+	update := func(now time.Time) {
+		reset(f)
+
+		// compute fraction through the period
+		progress := float32(now.Sub(now.Truncate(period))) / float32(period)
+		for i := range dots {
+			dots[i].Position = Tau*progress + float32(i)*divide
+			dots[i].Render(f)
+		}
+
+		DisplayLEDs(f)
+	}
+
+	t := time.NewTicker(time.Second / FrameRate)
 	for now := range t.C {
-		updaters = RunUpdaters(updaters, now)
+		update(now)
 	}
 
 	// <-context.Background().Done()
 }
 
+// DisplayLEDs puts a Frame out into the real world.
 func DisplayLEDs(data Frame) {
 	jsonData := make([]interface{}, len(data))
 	for i := range data {
@@ -103,36 +100,54 @@ func DisplayLEDs(data Frame) {
 	f.Invoke(jsonData)
 }
 
-// An UpdateFunc is any func meant to be called once per timer tick. An
-// UpdateFunc that returns true will be called again on the next tick, while one
-// that returns false will be discarded.
-type UpdateFunc = func(time.Time) bool
+// sprite is a one-dimensional object to be rendered onto a Frame.
+type sprite struct {
+	Color color.RGBA
+	// Center of the sprite in radians
+	Position float32
+	// Width of the sprite in radians
+	Size float32
+}
 
-// RunUpdaters runs each given UpdateFunc, and returns a slice of updaters that
-// should be called at the next tick.
-func RunUpdaters(fs []UpdateFunc, now time.Time) []UpdateFunc {
-	ret := make([]UpdateFunc, 0, len(fs))
-	for _, f := range fs {
-		if !f(now) {
-			continue
+// Render will overwrite the requisite pixels.
+// TODO: implement alpha channels?
+func (s sprite) Render(f Frame) {
+
+	start, end := s.Position-s.Size/2, s.Position+s.Size/2
+	firstPx := int(start / PixelWidth)
+	lastPx := int(0.5 + end/PixelWidth)
+	// fmt.Printf("start=%0.2f end=%0.2f firstPx=%v lastPx=%v\n", start, end, firstPx, lastPx)
+	// if lastPx >= FrameSize {
+	// 	fmt.Println("wrapping around")
+	// }
+	for i := firstPx; i <= lastPx; i++ {
+		fi := float32(i)
+		// overlap amount in radians
+		amt := overlap(start, end, fi*PixelWidth, (fi+1.0)*PixelWidth)
+		coverage := amt / PixelWidth // fraction of pixel covered
+		f[(len(f)+i)%len(f)] = RGB{
+			R: uint8(float32(s.Color.R) * coverage),
+			G: uint8(float32(s.Color.G) * coverage),
+			B: uint8(float32(s.Color.B) * coverage),
 		}
-		ret = append(ret, f)
 	}
-	return ret
 }
 
-// Tween computes a linear transition between two values over time.
-type Tween struct {
-	from, to   time.Time
-	start, end int32
-}
-
-// Value gets the current value of a Tween. It returns true if the Tween is not
-// yet finished.
-func (t Tween) Value(now time.Time) (int32, bool) {
-	if now.After(t.to) {
-		return t.end, false
+// overlap computes the size of overlap between ranges A and B. Returns 0.0 if
+// there is no overlap.
+func overlap(a1, a2, b1, b2 float32) float32 {
+	// Ensure that A starts to the left of B.
+	if b1 < a1 {
+		a1, a2, b1, b2 = b1, b2, a1, a2
 	}
-	frac := float32(now.Sub(t.from)) / float32(t.to.Sub(t.from))
-	return int32(float32(t.end-t.start)*frac) + t.start, true
+	// No overlap
+	if b1 > a2 {
+		return 0.0
+	}
+	// B is completely inside A; return size of B.
+	if b2 < a2 {
+		return b2 - b1
+	}
+	// Partial overlap
+	return a2 - b1
 }
