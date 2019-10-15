660310d3c2b1c4097779902deee6fc1913a91147
Jonathan Basseri <misterikkit@gmail.com>
2019-10-14 22:16:24 -0700

    get ready for different build types

---
+++ main_noop.go
@@ -0,0 +1,8 @@
+package main
+
+// +build !wasm
+
+// These are just to make the IDE happy
+
+func setup(*game)       {}
+func DisplayLEDs(Frame) {}
---
+++ setup_wasm.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+)
+
+// +build wasm
+
+func setup(g *game) {
+	fmt.Println("hello from main.go")
+	js.Global().Set("goHandleClick", js.FuncOf(handleClick(g)))
+	awaitWASMLoad()
+}
+
+func handleClick(g *game) func(js.Value, []js.Value) interface{} {
+	return func(_ js.Value, args []js.Value) interface{} {
+		id := args[0].String()
+		switch id {
+		case "timer_2m":
+			g.event(TIMER_2M)
+		case "timer_10m":
+			g.event(TIMER_10M)
+		case "cancel":
+			g.event(CANCEL)
+		}
+		return nil
+	}
+}
+
+func awaitWASMLoad() {
+	loaded := make(chan struct{})
+	js.Global().Set("goLoad", js.FuncOf(func(js.Value, []js.Value) interface{} { close(loaded); return nil }))
+	<-loaded
+}
+
+// DisplayLEDs puts a Frame out into the real world.
+func DisplayLEDs(data Frame) {
+	jsonData := make([]interface{}, len(data))
+	for i := range data {
+		jsonData[i] = map[string]interface{}{"R": data[i].R, "G": data[i].G, "B": data[i].B}
+	}
+	f := js.Global().Get("DisplayLEDs")
+	f.Invoke(jsonData)
+}
