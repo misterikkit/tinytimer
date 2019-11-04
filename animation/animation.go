8578845e93397dcd1d418089d084691a72c208de
Jonathan Basseri <misterikkit@gmail.com>
2019-11-03 23:01:23 -0800

    i do not like this other fixed point library

--- animation/animation.go
+++ animation/animation.go
@@ -5,24 +5,26 @@ import (
 	"time"
 
 	"github.com/misterikkit/tinytimer/graphics"
-	"golang.org/x/image/math/fixed"
+	"github.com/robaho/fixed"
 )
 
 // updateFn updates an animation based on the current time, and returns true if
 // the animation is complete.
 type updateFn = func(time.Time) bool
 
-type spinner struct {
+type Spinner struct {
 	Frame []color.RGBA
 	dots  []graphics.Sprite
 }
 
 const spinnerCount = 7
 
+var size = graphics.PixelWidth.Mul(fixed.MustParse("0.8"))
+var divide = graphics.Circ.Div(fixed.NewI(spinnerCount, 0))
+
 // NewSpinner initializes a spinner animation.
-func NewSpinner(c color.RGBA) spinner {
-	const size = graphics.PixelWidth * 8 / 10
-	s := spinner{
+func NewSpinner(c color.RGBA) Spinner {
+	s := Spinner{
 		Frame: make([]color.RGBA, graphics.FrameSize),
 		dots:  make([]graphics.Sprite, 0, spinnerCount),
 	}
@@ -33,19 +35,19 @@ func NewSpinner(c color.RGBA) spinner {
 }
 
 // Update computes the current frame of animation.
-func (s *spinner) Update(now time.Time) bool {
+func (s *Spinner) Update(now time.Time) bool {
 	const period = time.Second * spinnerCount
-	const divide = graphics.Circ / spinnerCount
 	graphics.Fill(s.Frame, graphics.Black)
 
 	// compute fraction through the period
-	elapsed := now.Sub(now.Truncate(period)).Nanoseconds()
-	period.Nanoseconds()
-	p := elapsed * 64 / period.Nanoseconds()
-	progress := fixed.Int26_6(p)
+	elapsed := fixed.NewI(now.Sub(now.Truncate(period)).Nanoseconds(), 0)
+	p := fixed.NewI(period.Nanoseconds(), 0)
+	progress := elapsed.Div(p)
+	// p := elapsed * 64 / period.Nanoseconds()
+	// progress := fixed.Int26_6(p)
 	// var progress fixed.Int26_6
 	for i := range s.dots {
-		s.dots[i].Position = (graphics.Circ.Mul(progress) + divide.Mul(fixed.I(i))) % graphics.Circ
+		s.dots[i].Position = (graphics.Circ.Mul(progress).Add(divide.Mul(fixed.NewI(int64(i), 0)))) // TODO: mod
 		s.dots[i].Render(s.Frame)
 	}
 	return false
--- graphics/graphics.go
+++ graphics/graphics.go
@@ -3,26 +3,26 @@ package graphics
 import (
 	"image/color"
 
-	"golang.org/x/image/math/fixed"
+	"github.com/robaho/fixed"
 )
 
 // Geometries
-const (
+var (
 	// Circ is 360 degrees
-	Circ = fixed.Int26_6(360 << 6)
+	Circ = fixed.NewI(360, 0)
 
 	// FrameSize is the number of pixels in one frame.
 	FrameSize = 24
 
 	// PixelWidth is the width in degrees of one pixel.
-	PixelWidth = Circ / FrameSize
+	PixelWidth = Circ.Div(fixed.NewI(int64(FrameSize), 0))
 )
-
-// Scale factor for LED intensity
-const toneItDown = fixed.Int26_6(1 << 6) // / 5
 
 // Colors
 var (
+	// Scale factor for LED intensity
+	toneItDown = fixed.NewI(1, 0) // / 5
+
 	Black = color.RGBA{}
 	White = scale(color.RGBA{0xFF, 0xFF, 0xFF, 0}, toneItDown)
 	Red   = scale(color.RGBA{0xFF, 0, 0, 0}, toneItDown)
@@ -36,59 +36,65 @@ var (
 type Sprite struct {
 	Color color.RGBA
 	// Center of the sprite in degrees
-	Position fixed.Int26_6
+	Position fixed.Fixed
 	// Width of the sprite in degrees
-	Size fixed.Int26_6
+	Size fixed.Fixed
 }
 
 // Render sets the sprite's pixels in the frame, rolling around the end of the
 // buffer, and blending with existing color values.
 func (s Sprite) Render(frame []color.RGBA) {
+	half := s.Size.Div(fixed.NewI(2, 0))
 	// Add Circ to position because math gets weird near zero.
-	start, end := Circ+s.Position-s.Size/2, Circ+s.Position+s.Size/2 // degrees
-	firstPx := int(start / PixelWidth)
-	lastPx := 1 + int(end/PixelWidth) // TODO: Do we need the extra 1?
+	start := Circ.Add(s.Position).Sub(half) // degrees
+	end := Circ.Add(s.Position).Add(half)   // degrees
+	firstPx := int(start.Div(PixelWidth).Int())
+	lastPx := 1 + int(end.Div(PixelWidth).Int()) // TODO: Do we need the extra 1?
 
 	for i := firstPx; i <= lastPx; i++ {
+		fi := fixed.NewI(int64(i), 0)
 		// amount of overlap between sprite and current pixel in degrees
-		pxStart := fixed.I(i).Mul(PixelWidth)      // degrees
-		pxEnd := fixed.I(i + 1).Mul(PixelWidth)    // degrees
+		pxStart := fi.Mul(PixelWidth)              // degrees
+		pxEnd := pxStart.Add(PixelWidth)           // degrees
 		amt := overlap(start, end, pxStart, pxEnd) // degrees
-		coverage := amt * 64 / PixelWidth          // fraction of pixel covered
+		coverage := amt.Div(PixelWidth)            // fraction of pixel covered
 		// When rendering partial coverage, blend the color with the existing color.
 		index := (len(frame) + i) % len(frame)
-		frame[index] = add(scale(s.Color, coverage), scale(frame[index], fixed.I(1)-coverage))
+		frame[index] = add(
+			scale(s.Color, coverage),
+			scale(frame[index], fixed.NewI(1, 0).Sub(coverage)),
+		)
 	}
 }
 
 // overlap computes the size of overlap between ranges A and B. Returns 0.0 if
 // there is no overlap.
-func overlap(a1, a2, b1, b2 fixed.Int26_6) fixed.Int26_6 {
+func overlap(a1, a2, b1, b2 fixed.Fixed) fixed.Fixed {
 	// Ensure that A starts to the left of B.
-	if b1 < a1 {
+	if b1.LessThan(a1) {
 		a1, a2, b1, b2 = b1, b2, a1, a2
 	}
 	// No overlap
-	if b1 > a2 {
-		return 0
+	if b1.GreaterThan(a2) {
+		return fixed.ZERO
 	}
 	// B is completely inside A; return size of B.
-	if b2 < a2 {
-		return b2 - b1
+	if b2.LessThan(a2) {
+		return b2.Sub(b1)
 	}
 	// Partial overlap
-	return a2 - b1
+	return a2.Sub(b1)
 }
 
 // scale performs alpha multiplying on a scale of 0 to 1, rather than 0 to 255.
-func scale(c color.RGBA, s fixed.Int26_6) color.RGBA {
-	r := s.Mul(fixed.I(int(c.R)))
-	g := s.Mul(fixed.I(int(c.G)))
-	b := s.Mul(fixed.I(int(c.B)))
+func scale(c color.RGBA, s fixed.Fixed) color.RGBA {
+	r := s.Mul(fixed.NewI(int64(c.R), 0))
+	g := s.Mul(fixed.NewI(int64(c.G), 0))
+	b := s.Mul(fixed.NewI(int64(c.B), 0))
 	return color.RGBA{
-		R: uint8(r.Round()),
-		G: uint8(g.Round()),
-		B: uint8(b.Round()),
+		R: uint8(r.Int()),
+		G: uint8(g.Int()),
+		B: uint8(b.Int()),
 		A: c.A,
 	}
 }
