f9754b56509134e2b1b0adf8fb7cb9f7ff2cb99a
Jonathan Basseri <misterikkit@gmail.com>
2019-11-06 23:23:18 -0800

    getting a start on interrupts again

--- setup_m4.go
+++ setup_m4.go
@@ -57,3 +57,38 @@ func enableFPU() {
 	// http://infocenter.arm.com/help/topic/com.arm.doc.ddi0439b/DDI0439B_cortex_m4_r0p0_trm.pdf
 	sam.SystemControl.CPACR.SetBits(0xF << 20)
 }
+
+func configureExternalInterrupt() {
+	// 0. Disable EIC
+	sam.EIC.CTRLA.ClearBits(sam.EIC_CTRLA_ENABLE)
+	for sam.EIC.SYNCBUSY.Get() != 0 {
+	}
+
+	// 1. Enable CLK_EIC_APB
+	// Should be enabled by default, but let's be safe
+	sam.MCLK.APBAMASK.SetBits(sam.MCLK_APBAMASK_EIC_)
+
+	// 2. If required, configure the NMI by writing the Non-Maskable Interrupt Control register (NMICTRL)
+
+	// 3. Enable GCLK_EIC or CLK_ULP32K when one of the following configuration is selected:
+	// – the NMI uses edge detection or filtering.
+	// – one EXTINT uses filtering.
+	// – one EXTINT uses synchronous edge detection.
+	// – one EXTINT uses debouncing.
+	// GCLK_EIC is used when a frequency higher than 32KHz is required for filtering.
+	// CLK_ULP32K is recommended when power consumption is the priority. For CLK_ULP32K write a
+	// '1' to the Clock Selection bit in the Control A register (CTRLA.CKSEL).
+	sam.EIC.CTRLA.SetBits(sam.EIC_CTRLA_CKSEL_CLK_ULP32K << sam.EIC_CTRLA_CKSEL_Pos)
+
+	// 4. Configure the EIC input sense and filtering by writing the Configuration n register (CONFIG).
+	// TODO:
+	// D2==PA07==EXTINT[7], D11==PA21==EXTINT[5], D12==PA23==EXTINT[7]
+
+	// 5. Optionally, enable the asynchronous mode.
+	// 6. Optionally, enable the debouncer mode.
+
+	// 7. Enable the EIC by writing a ‘1’ to CTRLA.ENABLE.
+	sam.EIC.CTRLA.SetBits(sam.EIC_CTRLA_ENABLE)
+	for sam.EIC.SYNCBUSY.Get() != 0 {
+	}
+}
