ad38db17059af8e0966fae608ca615a2573efc1d
Jonathan Basseri <misterikkit@gmail.com>
2019-11-10 16:19:42 -0800

    the DotStar is disabled!

--- setup_m4.go
+++ setup_m4.go
@@ -9,11 +9,11 @@ import (
 
 	"github.com/misterikkit/tinytimer/game"
 	"github.com/misterikkit/tinytimer/ws2812"
+	"tinygo.org/x/drivers/apa102"
 )
 
 type userInterface struct {
 	neoPix    ws2812.Device
-	led       machine.Pin
 	btnCancel machine.Pin
 	btn2Min   machine.Pin
 	btn10Min  machine.Pin
@@ -21,8 +21,9 @@ type userInterface struct {
 
 func setup(g *game.Game) userInterface {
 	enableFPU()
+	turnOffDotStar()
+
 	neoPin := machine.D5
-	makeOutput(machine.LED)
 	makeOutput(neoPin)
 	neoPix := ws2812.New(neoPin)
 
@@ -42,7 +43,8 @@ func setup(g *game.Game) userInterface {
 			g.Event(game.TIMER_10M)
 		}
 	}
-	return userInterface{neoPix, machine.LED, btnCancel, btn2Min, btn10Min}
+
+	return userInterface{neoPix, btnCancel, btn2Min, btn10Min}
 }
 
 func (f *userInterface) DisplayLEDs(c []color.RGBA) {
@@ -56,4 +58,12 @@ func enableFPU() {
 	// See section 7.3.1 in
 	// http://infocenter.arm.com/help/topic/com.arm.doc.ddi0439b/DDI0439B_cortex_m4_r0p0_trm.pdf
 	sam.SystemControl.CPACR.SetBits(0xF << 20)
+}
+
+// turnOffDotStar writes a zero value to the on-board RGB LED to save power.
+func turnOffDotStar() {
+	onboardDotStar := apa102.NewSoftwareSPI(machine.PB02, machine.PB03, 100)
+	onboardDotStar.WriteColors([]color.RGBA{{}})
+	// 50% of the time, it works all of the time. ¯\_(ツ)_/¯
+	onboardDotStar.WriteColors([]color.RGBA{{}})
 }
