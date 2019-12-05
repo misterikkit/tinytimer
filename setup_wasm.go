a9c46c3d91a54b87611b9f40b4aee6b981b0c1a7
Jonathan Basseri <misterikkit@gmail.com>
2019-12-04 23:42:49 -0800

    wasm buttons are working!!

--- setup_wasm.go
+++ setup_wasm.go
@@ -5,46 +5,36 @@ package main
 import (
 	"image/color"
 	"syscall/js"
-
-	"github.com/misterikkit/tinytimer/apps/timer"
 )
 
 type userInterface struct {
-	btnCancel bool
-	btn2Min   bool
-	btn10Min  bool
+	pollCancel js.Value
+	poll2Min   js.Value
+	poll10Min  js.Value
+	display    js.Value
 }
 
-func (ui *userInterface) BtnCancel() bool { return ui.btnCancel }
-func (ui *userInterface) Btn2Min() bool   { return ui.btn2Min }
-func (ui *userInterface) Btn10Min() bool  { return ui.btn10Min }
-
-func (ui *userInterface) Sleepish() { /*TODO*/ }
-
 func setup() *userInterface {
-	ui := new(userInterface)
-	// js.Global().Set("goHandleClick", js.FuncOf(handleClick(g)))
+	ui := &userInterface{
+		pollCancel: js.Global().Get("ButtonCancel"),
+		poll2Min:   js.Global().Get("Button2M"),
+		poll10Min:  js.Global().Get("Button10M"),
+		display:    js.Global().Get("DisplayLEDs"),
+	}
 	return ui
 }
 
-func handleClick(g *timer.App) func(js.Value, []js.Value) interface{} {
-	return func(_ js.Value, args []js.Value) interface{} {
-		id := args[0].String()
-		switch id {
-		case "timer_2m":
-		case "timer_10m":
-		case "cancel":
-		}
-		return nil
-	}
-}
+func (ui *userInterface) BtnCancel() bool { return ui.pollCancel.Invoke().String() == "true" }
+func (ui *userInterface) Btn2Min() bool   { return ui.poll2Min.Invoke().String() == "true" }
+func (ui *userInterface) Btn10Min() bool  { return ui.poll10Min.Invoke().String() == "true" }
+
+func (ui *userInterface) Sleepish() { /*TODO*/ }
 
 // DisplayLEDs puts a Frame out into the real world.
-func (userInterface) DisplayLEDs(data []color.RGBA) {
+func (ui *userInterface) DisplayLEDs(data []color.RGBA) {
 	jsonData := make([]interface{}, len(data))
 	for i := range data {
 		jsonData[i] = map[string]interface{}{"R": data[i].R, "G": data[i].G, "B": data[i].B}
 	}
-	f := js.Global().Get("DisplayLEDs")
-	f.Invoke(jsonData)
+	ui.display.Invoke(jsonData)
 }
---
+++ wasm_site/js/button.js
@@ -0,0 +1,4 @@
+function ButtonCancel() { return $("#timer_cancel").attr("state"); }
+function Button2M() { return $("#timer_2m").attr("state"); }
+function Button10M() { return $("#timer_10m").attr("state"); }
+
